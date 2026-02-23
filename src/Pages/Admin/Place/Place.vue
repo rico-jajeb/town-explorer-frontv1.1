9f4393a32f78ddd9bd092f4110302cb8ff777ca6
Jeb <ricojajeb.tayobong@gmail.com>
2026-02-23 23:30:11 +0800

    feat(placeCard): create a place card status

--- src/Pages/Admin/Place/Place.vue
+++ src/Pages/Admin/Place/Place.vue
@@ -59,14 +59,129 @@
     <section class="mt-4 flex justify-between">
       <div>
         <h1 class="mb-2 font-bold text-2xl">Places</h1>
-        <p class="text-body">Pastrana's Most Visit Place</p>
+        <p class="text-body">Manage and monitor all locationin your town</p>
       </div>
-      <div class="">
-        <button class="bg-blue-400 px-4 py-2 rounded rounded-2">Add Place</button>
+      <div class="flex gap-4 items-center">
+        <form>
+          <label for="search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+            >Search</label
+          >
+          <div class="relative">
+            <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+              <svg
+                class="w-4 h-4 text-body"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-width="2"
+                  d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z"
+                />
+              </svg>
+            </div>
+            <input
+              type="search"
+              id="search"
+              class="block w-full p-3 ps-9 bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand shadow-xs placeholder:text-body"
+              placeholder="Search"
+              required
+            />
+          </div>
+        </form>
+
+        <button
+          id="dropdownDefaultButton"
+          data-dropdown-toggle="dropdown"
+          class="h-12 shrink-0 inline-flex items-center justify-center text-body bg-neutral-secondary-medium box-border border border-default-medium hover:bg-neutral-tertiary-medium hover:text-heading focus:ring-4 focus:ring-neutral-tertiary shadow-xs font-medium leading-5 rounded-base text-sm px-3 py-2 focus:outline-none"
+          type="button"
+        >
+          <svg
+            class="w-4 h-4 me-1.5 -ms-0.5"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-width="2"
+              d="M18.796 4H5.204a1 1 0 0 0-.753 1.659l5.302 6.058a1 1 0 0 1 .247.659v4.874a.5.5 0 0 0 .2.4l3 2.25a.5.5 0 0 0 .8-.4v-7.124a1 1 0 0 1 .247-.659l5.302-6.059c.566-.646.106-1.658-.753-1.658Z"
+            />
+          </svg>
+          Filter by
+          <svg
+            class="w-4 h-4 ms-1.5 -me-0.5"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="m19 9-7 7-7-7"
+            />
+          </svg>
+        </button>
+        <!-- Dropdown menu -->
+        <div
+          id="dropdown"
+          class="z-10 hidden bg-neutral-primary-medium border border-default-medium rounded-base shadow-lg w-32"
+        >
+          <ul class="p-2 text-sm text-body font-medium" aria-labelledby="dropdownDefaultButton">
+            <li>
+              <a
+                href="#"
+                class="inline-flex items-center w-full p-2 hover:bg-neutral-tertiary-medium hover:text-heading rounded"
+                >Color</a
+              >
+            </li>
+            <li>
+              <a
+                href="#"
+                class="inline-flex items-center w-full p-2 hover:bg-neutral-tertiary-medium hover:text-heading rounded"
+                >Category</a
+              >
+            </li>
+            <li>
+              <a
+                href="#"
+                class="inline-flex items-center w-full p-2 hover:bg-neutral-tertiary-medium hover:text-heading rounded"
+                >Price</a
+              >
+            </li>
+            <li>
+              <a
+                href="#"
+                class="inline-flex items-center w-full p-2 hover:bg-neutral-tertiary-medium hover:text-heading rounded"
+                >Sign out</a
+              >
+            </li>
+          </ul>
+        </div>
+
+        <button class="bg-blue-400 px-3 py-2 h-12 rounded rounded-2">Add Place</button>
       </div>
     </section>
 
-    <section>
+    <section class="mt-8">
+      <PlaceCard />
+    </section>
+
+    <section class="mt-8">
       <PlaceMap />
     </section>
   </main>
@@ -74,4 +189,5 @@
 
 <script setup lang="ts">
 import PlaceMap from '@/components/Maps/PlaceMap.vue'
+import PlaceCard from './PlaceCard.vue'
 </script>
---
+++ src/Pages/Admin/Place/PlaceCard.vue
@@ -0,0 +1,99 @@
+<template>
+  <main class="grid grid-cols-8 gap-4">
+    <div
+      class="sm:col-span-2 col-span-8 bg-neutral-primary-soft block max-w-sm p-6 border border-default rounded-base shadow-xs"
+    >
+      <span
+        ><svg
+          class="w-8 h-8 text-gray-800 dark:text-white"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          fill="currentColor"
+          viewBox="0 0 24 24"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M11.906 1.994a8.002 8.002 0 0 1 8.09 8.421 7.996 7.996 0 0 1-1.297 3.957.996.996 0 0 1-.133.204l-.108.129c-.178.243-.37.477-.573.699l-5.112 6.224a1 1 0 0 1-1.545 0L5.982 15.26l-.002-.002a18.146 18.146 0 0 1-.309-.38l-.133-.163a.999.999 0 0 1-.13-.202 7.995 7.995 0 0 1 6.498-12.518ZM15 9.997a3 3 0 1 1-5.999 0 3 3 0 0 1 5.999 0Z"
+            clip-rule="evenodd"
+          />
+        </svg>
+      </span>
+
+      <h3 class="mt-4 mb-1 text-2xl font-bold">1,234</h3>
+      <p class="text-body">Total Places</p>
+    </div>
+    <div
+      class="sm:col-span-2 col-span-8 bg-neutral-primary-soft block max-w-sm p-6 border border-default rounded-base shadow-xs"
+    >
+      <span
+        ><svg
+          class="w-8 h-8 text-gray-800 dark:text-white"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          fill="currentColor"
+          viewBox="0 0 24 24"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm11-4a1 1 0 1 0-2 0v4a1 1 0 0 0 .293.707l3 3a1 1 0 0 0 1.414-1.414L13 11.586V8Z"
+            clip-rule="evenodd"
+          />
+        </svg>
+      </span>
+
+      <h3 class="mt-4 mb-1 text-2xl font-bold">1,234</h3>
+      <p class="text-body">Pending Review</p>
+    </div>
+    <div
+      class="sm:col-span-2 col-span-8 bg-neutral-primary-soft block max-w-sm p-6 border border-default rounded-base shadow-xs"
+    >
+      <span
+        ><svg
+          class="w-8 h-8 text-gray-800 dark:text-white"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          fill="currentColor"
+          viewBox="0 0 24 24"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm13.707-1.293a1 1 0 0 0-1.414-1.414L11 12.586l-1.793-1.793a1 1 0 0 0-1.414 1.414l2.5 2.5a1 1 0 0 0 1.414 0l4-4Z"
+            clip-rule="evenodd"
+          />
+        </svg>
+      </span>
+
+      <h3 class="mt-4 mb-1 text-2xl font-bold">1,234</h3>
+      <p class="text-body">Verified</p>
+    </div>
+    <div
+      class="sm:col-span-2 col-span-8 bg-neutral-primary-soft block max-w-sm p-6 border border-default rounded-base shadow-xs"
+    >
+      <span
+        ><svg
+          class="w-8 h-8 text-gray-800 dark:text-white"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          fill="currentColor"
+          viewBox="0 0 24 24"
+        >
+          <path
+            d="M13.849 4.22c-.684-1.626-3.014-1.626-3.698 0L8.397 8.387l-4.552.361c-1.775.14-2.495 2.331-1.142 3.477l3.468 2.937-1.06 4.392c-.413 1.713 1.472 3.067 2.992 2.149L12 19.35l3.897 2.354c1.52.918 3.405-.436 2.992-2.15l-1.06-4.39 3.468-2.938c1.353-1.146.633-3.336-1.142-3.477l-4.552-.36-1.754-4.17Z"
+          />
+        </svg>
+      </span>
+
+      <h3 class="mt-4 mb-1 text-2xl font-bold">1,234</h3>
+      <p class="text-body">AI Recommended</p>
+    </div>
+  </main>
+</template>
+<script setup></script>
